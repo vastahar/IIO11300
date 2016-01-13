0a58cc6d2fdfcd455b6d74ee37b746b42ea111bd
H9096 <H9096@student.jamk.fi>
2016-01-13 10:29:37 +0200

    Modify MainWindow

--- IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
@@ -1,8 +1,8 @@
 ﻿/*
 * Copyright (C) JAMK/IT/Esa Salmikangas
 * This file is part of the IIO11300 course project.
-* Created: 12.1.2015
-* Authors: Esa Salmikangas
+* Created: 12.1.2016 Modified: 13.1.2016
+* Authors: Harri Vastamäki, Esa Salmikangas
 */using System;
 using System.Collections.Generic;
 using System.Linq;
